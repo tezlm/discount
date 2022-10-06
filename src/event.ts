00000110a50ca05f383c127e65905424d5806edb
tezlm <zestylemonade@celery.eu.org>
2022-10-06 12:29:35 -0700

    fixes

--- src/event.ts
+++ src/event.ts
@@ -1,4 +1,5 @@
 import type Room from "./room";
+import type Member from "./member";
 
 export interface RawEvent {
   event_id: string,
@@ -19,17 +20,50 @@ export interface RawEphemeralEvent {
   type: string,
 }
 
+export interface Relation {
+  event: Event,
+  relType: string,
+}
+
 export class Event<RawType extends RawEvent = RawEvent> {  
   public client = this.room.client;
-  // public relationsIn: Array<Event> = [];
-  // public relationsOut: Array<Event> = [];
-  // private cacheContent: any = {};
+  public relationsIn:  Array<Relation> | null = null; // events pointing to me
+  public relationsOut: Array<Relation> | null = null; // events i pont to
+  private _contentCache: any = null;
+  
+  // TEMP: discard compat
+  public flags = new Set();
+  public reactions = null; // move to getter?
   
   constructor(
     public room: Room,
     public raw: RawType,
-  ) {}  
+  ) {}
   
+  parseRelation(event: Event, relType: string) {
+    this._handleRelation(event, relType);
+  }
+  
+  _handleRelation(event: Event, relType: string) {
+    if (relType === "m.replace") {
+      if (event.raw.sender !== this.raw.sender) return;
+      this._contentCache = null;
+      this.flags.add("edited");
+    }
+    
+    if (event.relationsOut === null) {
+      event.relationsOut = [{ event: this, relType }];
+    } else {
+      event.relationsOut.push({ event: this, relType });
+    }
+    
+    if (this.relationsIn === null) {
+      this.relationsIn = [{ event, relType }];
+    } else {
+      this.relationsIn.push({ event, relType });
+    }
+  }
+    
   get id(): string {
     return this.raw.event_id;
   }  
@@ -38,12 +72,23 @@ export class Event<RawType extends RawEvent = RawEvent> {
     return this.raw.type;
   }
   
-  get sender(): string {
-    return this.raw.sender;
+  get sender(): Member {
+    const member = this.room.members.get(this.raw.sender);
+    if (!member) throw "could not find member";
+    return member;
   }
   
+  // should handle edits, e2ee, and legacy events
   get content(): any {
-    return this.raw.content;
+    if (this._contentCache) return this._contentCache;
+    
+    const edit = this.relationsIn?.find(i => i.relType === "m.replace");   
+    const content = edit
+      ? { ...edit.event.content["m.new_content"], "m.relates_to": this.raw.content["m.relates_to"] }
+      : this.raw.content;
+    
+    this._contentCache = content;
+    return content;
   }
   
   get unsigned(): any {
@@ -73,7 +118,8 @@ export class Event<RawType extends RawEvent = RawEvent> {
   
   // TEMP: discard compat
   get eventId(): string { return this.raw.event_id }
-  get roomId(): string { return this.room.id }
+  get roomId(): string { return this.room.id }  
+  get date(): Date { return this.timestamp }  
 }
 
 export class StateEvent extends Event<RawStateEvent> {  
