6c12706e5847646bc93b9c2179b266040f70c6a6
tezlm <zestylemonade@celery.eu.org>
2022-10-09 05:53:19 -0700

    compat for discard f9a9c75

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import Client from "./client.js";
-import Room from "./room.js";
-import { Event, StateEvent } from "./event.js";
+import Client from "./client";
+import Room from "./room";
+import { Event, StateEvent } from "./event";
+import Member from "./member";
 
 export {
   Client,
   Room,
   Event,
   StateEvent,
+  Member,
 }
